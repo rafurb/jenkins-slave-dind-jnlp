596179012c3e985d80c4b4cc8b1502f35b7e782c
rafurb <rafal.urbas@gmail.com>
2020-04-26 23:39:48 +0200

    Update entrypoint.sh3

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ echo "starting dockerd..."
 sudo dockerd --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 --storage-driver=vfs &
 echo "starting jnlp slave..."
 exec java -jar /usr/share/jenkins/slave.jar \
-	-jnlpUrl $JENKINS_URL/computer/$JENKINS_SLAVE_NAME/slave-agent.jnlp \
+	-jnlpUrl $JENKINS_MASTER/computer/$JENKINS_SLAVE_NAME/slave-agent.jnlp \
 	-secret $JENKINS_SLAVE_SECRET
